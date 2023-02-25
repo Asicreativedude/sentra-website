b409cbf68440359440659e1790eba1cc7ff433fe
Asicreativedude <Asicreativedude@gmail.com>
2023-02-25 18:46:11 +0200

    added video blog logic

---
+++ video-blog-script.js
@@ -0,0 +1,87 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+	updateNavbar();
+};
+
+// Get the header
+let header = document.getElementById('navBar');
+
+// Get the offset position of the navbar
+let sticky = header.offsetTop;
+function traverseChildrenNoSticky(node) {
+	node.classList.remove('silent-nav');
+	for (let i = 0; i < node.children.length; i++) {
+		traverseChildrenNoSticky(node.children[i]);
+	}
+}
+function traverseChildrenToSticky(node) {
+	node.classList.add('silent-nav');
+	for (let i = 0; i < node.children.length; i++) {
+		traverseChildrenToSticky(node.children[i]);
+	}
+}
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function updateNavbar() {
+	if (window.pageYOffset > sticky) {
+		header.classList.remove('silent-nav');
+		traverseChildrenNoSticky(header);
+	} else {
+		header.classList.add('silent-nav');
+		traverseChildrenToSticky(header);
+	}
+}
+
+// resources tags text color
+let tags = document.querySelectorAll('.resources-tag-w');
+for (const tag of tags) {
+	if (
+		tag.style.backgroundColor == 'rgb(23, 184, 144)' ||
+		tag.style.backgroundColor == 'rgb(65, 191, 232)' ||
+		tag.style.backgroundColor == 'rgb(245, 233, 96)'
+	) {
+		tag.children[0].classList.remove('white-text');
+	}
+}
+
+const player = Plyr.setup('#player', {
+	controls: [
+		'play-large',
+		'play',
+		'progress',
+		'current-time',
+		'mute',
+		'volume',
+		'captions',
+		'settings',
+		'pip',
+		'airplay',
+	],
+});
+
+// Register IntersectionObserver
+const io = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		if (entry.intersectionRatio > 0 && entry.target.style.display != 'none') {
+			// Add 'active' class if observation target is inside viewport
+			header.style.zIndex = '84';
+		} else {
+			// Remove 'active' class otherwise
+			header.style.zIndex = '1000';
+		}
+	});
+});
+
+// Declares what to observe, and observes its properties.
+const videos = document.querySelectorAll('.video-popup-c');
+videos.forEach((el) => {
+	io.observe(el);
+});
+
+let closeBtn = document.querySelectorAll('.video-popup-close-btn');
+closeBtn.forEach((i) => {
+	i.addEventListener('click', () => {
+		player.forEach((e) => {
+			e.pause();
+		});
+	});
+});
