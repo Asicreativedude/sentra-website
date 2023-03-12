83a934ef3a6d6368a397f4f226736a865cb996d1
Asicreativedude <Asicreativedude@gmail.com>
2023-03-12 19:27:49 +0200

    careerpage marquee updates

--- career-page-script.js
+++ career-page-script.js
@@ -14,59 +14,15 @@ newListItem.appendChild(circleDefault);
 let imagesWrapper = document.querySelectorAll(
 	'.career-images-collection > .employee-pictures-w > .w-dyn-item'
 );
+
 function addCircleBetweenEveryOtherImage() {
+	let colors = ['red', 'black', 'yellow', 'purple'];
 	imagesWrapper.forEach((image, index) => {
 		let circle = newListItem.cloneNode(true);
-		switch (index) {
-			case 0:
-				circle.children[0].classList.add('purple');
-				break;
-			case 1:
-				circle.children[0].classList.add('black');
-				break;
-			case 2:
-				circle.children[0].classList.add('yellow');
-				break;
-			case 3:
-				circle.children[0].classList.add('red');
-				break;
-			case 4:
-				circle.children[0].classList.add('purple');
-				break;
-			case 5:
-				circle.children[0].classList.add('black');
-				break;
-			case 6:
-				circle.children[0].classList.add('yellow');
-				break;
-			case 7:
-				circle.children[0].classList.add('red');
-				break;
-			case 8:
-				circle.children[0].classList.add('purple');
-				break;
-			case 9:
-				circle.children[0].classList.add('black');
-				break;
-			case 10:
-				circle.children[0].classList.add('yellow');
-				break;
-			case 11:
-				circle.children[0].classList.add('red');
-				break;
-			case 12:
-				circle.children[0].classList.add('purple');
-				break;
-			case 13:
-				circle.children[0].classList.add('black');
-				break;
-			case 14:
-				circle.children[0].classList.add('yellow');
-				break;
-			case 15:
-				circle.children[0].classList.add('red');
-				break;
-		}
+		let circleColor = colors[index % colors.length];
+
+		circle.children[0].classList.add(circleColor);
+		console.log(circle.children[0].classList);
 		for (let i = 0; i < imageLists.length; i++) {
 			if (imageLists[i].contains(image)) {
 				imageLists[i].insertBefore(circle, image);
@@ -75,6 +31,15 @@ function addCircleBetweenEveryOtherImage() {
 	});
 }
 
+let employeeDetails = document.querySelectorAll('.employee-details');
+function addRandomColorToEmployeeDetails() {
+	let colors = ['#feb236', '#f88286', '#b9abff'];
+	employeeDetails.forEach((detail, index) => {
+		let color = colors[index % colors.length];
+		detail.style.backgroundColor = color;
+	});
+}
+addRandomColorToEmployeeDetails();
 addCircleBetweenEveryOtherImage();
 
 let employeeImages = document.querySelectorAll('.employee-picture-c');
@@ -88,3 +53,14 @@ for (let i = 0; i < employeeImages.length; i++) {
 		employeeImages[i].children[1].classList.toggle('flipped');
 	});
 }
+
+function randomlyFlipEmployeeImages() {
+	employeeImages.forEach((image) => {
+		let random = Math.floor(Math.random() * 8);
+		if (random === 1) {
+			image.children[0].classList.toggle('flipped');
+			image.children[1].classList.toggle('flipped');
+		}
+	});
+}
+randomlyFlipEmployeeImages();
